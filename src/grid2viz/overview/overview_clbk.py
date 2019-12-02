7df8dcbd1372c8e8dd2ca720e451dac5698bcdce
mmohandi <maxime.mohandi@artelys.com>
2019-12-02 16:41:03 +0100

    callback overview test

--- src/grid2viz/overview/overview_clbk.py
+++ src/grid2viz/overview/overview_clbk.py
@@ -1,8 +1,8 @@
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-
+import plotly.graph_objects as go
 from src.app import app
-import numpy as np  # TODO delete after getting the back end working
+import numpy as np
 
 # TODO remove this when callback created
 N = 100
@@ -11,24 +11,18 @@ random_y0 = np.random.randn(N) + 5
 random_y1 = np.random.randn(N)
 random_y2 = np.random.randn(N)
 
-@app.callback(
-    Output("indicator_line_charts", "figure"),
-    [Input("store", "data")],
-    [State("indicator_line_charts", "figure")]
-)
-def load_indicators_data(store, figure):
-    print("test")
-    if figure is None or store is None:
-        raise PreventUpdate
-    data = [dict(
-        x=random_x, y=random_y1
-    )]
-    return {'data': data}
 
-
-
-def load_summary_data():
+def load_indicators_data(data, figure):
     pass
+
+
+@app.callback(
+    Output("input_env_charts", "figure"),
+    [Input("input_env_selector", "value")]
+)
+def load_summary_data(value):
+    print(value)
+    return {'data': [go.Scatter(x=random_x, y=random_y0)]}
 
 
 def load_ref_agent_data():
