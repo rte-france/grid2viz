1f2dbc9076bf294d1170e23f808c41da64fbd86d
mmohandi <maxime.mohandi@artelys.com>
2020-01-21 14:45:31 +0100

    voltage timeserie

--- src/grid2viz/micro/micro_clbk.py
+++ src/grid2viz/micro/micro_clbk.py
@@ -1,8 +1,155 @@
-from dash.dependencies import Output, Input, State
-
+from dash.dependencies import Input, Output, State
 from src.app import app
-from src.grid2viz.utils.graph_utils import relayout_callback
+import pandas as pd
+import plotly.graph_objects as go
+from src.grid2viz.utils.graph_utils import relayout_callback, get_axis_relayout
+from src.grid2kpi.episode import observation_model, env_actions, profiles_traces
+from src.grid2kpi.manager import episode, make_episode, base_dir, indx, agent_ref, prod_types
 
+
+@app.callback(
+    Output("relayoutStoreMicro", "data"),
+    [Input("env_charts_ts", "relayoutData"),
+     Input("usage_rate_ts", "relayoutData"),
+     Input("overflow_ts", "relayoutData")],
+    [State("relayoutStoreMicro", "data")]
+)
+def relayout_store_overview(*args):
+    return relayout_callback(*args)
+
+
+# flux line callback
+@app.callback(
+    [Output('line_side_choices', 'options'),
+     Output('line_side_choices', 'value')],
+    [Input('voltage_flow_selector', 'value')]
+)
+def load_voltage_flow_line_choice(value):
+    option = []
+    for names in episode.line_names:
+        option.append({
+            'label': 'ex_' + names,
+            'value': 'ex_' + names
+        })
+        option.append({
+            'label': 'or_' + names,
+            'value': 'or_' + names
+        })
+    return option, [option[0]['value']]
+
+
+@app.callback(
+    Output('voltage_flow_graph', 'figure'),
+    [Input('line_side_choices', 'value')],
+    [State('voltage_flow_graph', 'figure')]
+)
+def load_flow_voltage_graph(values, figure):
+    if values is not None:
+        voltage_ex = pd.DataFrame([obs.v_ex for obs in episode.observations], columns=episode.line_names)
+        voltage_or = pd.DataFrame([obs.v_or for obs in episode.observations], columns=episode.line_names)
+        traces = []
+
+        for value in values:
+            line_side = str(value)[:2]  # the first 2 characters are the side of line ('ex' or 'or')
+            line_name = str(value)
+            if line_side == 'ex':
+                traces.append(go.Scatter(
+                    x=episode.timestamps,
+                    y=voltage_ex[line_name[3:]].round(3),  # remove the first 3 char to get the line name and round to 3 decimals
+                    name=line_name)
+                )
+            if line_side == 'or':
+                traces.append(go.Scatter(
+                    x=episode.timestamps,
+                    y=voltage_or[line_name[3:]].round(3),
+                    name=line_name)
+                )
+
+        figure['data'] = traces
+
+    return figure
+
+
+# context line callback
+
+@app.callback(
+    [Output("asset_selector", "options"),
+     Output("asset_selector", "value")],
+    [Input("environment_choices_buttons", "value")]
+)
+def update_ts_graph_avail_assets(kind):
+    if kind in ["Hazards", "Maintenances"]:
+        options, value = [{'label': line_name, 'value': line_name}
+                          for line_name in [*episode.line_names, 'total']], episode.line_names[0]
+    elif kind == 'Production':
+        options = [{'label': prod_name,
+                    'value': prod_name}
+                   for prod_name in [*episode.prod_names, *list(set(prod_types.values())), 'total']]
+        value = episode.prod_names[0]
+    else:
+        options = [{'label': load_name,
+                    'value': load_name}
+                   for load_name in [*episode.load_names, 'total']]
+        value = episode.load_names[0]
+
+    return options, value
+
+
+@app.callback(
+    Output("env_charts_ts", "figure"),
+    [Input("asset_selector", "value"),
+     Input("relayoutStoreMicro", "data")],
+    [State("env_charts_ts", "figure"),
+     State("environment_choices_buttons", "value")]
+)
+def load_context_data(equipments, relayout_data_store, figure, kind):
+    if relayout_data_store is not None and relayout_data_store["relayout_data"]:
+        relayout_data = relayout_data_store["relayout_data"]
+        layout = figure["layout"]
+        new_axis_layout = get_axis_relayout(figure, relayout_data)
+        if new_axis_layout is not None:
+            layout.update(new_axis_layout)
+            return figure
+    if kind is None:
+        return figure
+    if isinstance(equipments, str):
+        equipments = [equipments]  # to make pd.series.isin() work
+
+    if kind == "Load":
+        figure["data"] = observation_model.get_load_trace_per_equipment(equipments)
+    if kind == "Production":
+        figure["data"] = observation_model.get_all_prod_trace(equipments)
+    if kind == "Hazards":
+        figure["data"] = observation_model.get_hazard_trace(equipments)
+    if kind == "Maintenances":
+        figure["data"] = observation_model.get_maintenance_trace(equipments)
+
+    return figure
+
+
+@app.callback(
+    [Output("overflow_ts", "figure"), Output("usage_rate_ts", "figure")],
+    [Input('agent_selector', 'value'),
+     Input("relayoutStoreMicro", "data")],
+    [State("overflow_ts", "figure"), State("usage_rate_ts", "figure")]
+)
+def update_agent_ref_graph(value, relayout_data_store, figure_overflow, figure_usage):
+    if relayout_data_store is not None and relayout_data_store["relayout_data"]:
+        relayout_data = relayout_data_store["relayout_data"]
+        layout_usage = figure_usage["layout"]
+        new_axis_layout = get_axis_relayout(figure_usage, relayout_data)
+        if new_axis_layout is not None:
+            layout_usage.update(new_axis_layout)
+            figure_overflow["layout"].update(new_axis_layout)
+            return figure_overflow, figure_usage
+    if value == agent_ref:
+        new_episode = episode
+    else:
+        new_episode = make_episode(base_dir, value, indx)
+    figure_overflow["data"] = observation_model.get_total_overflow_trace(
+        new_episode)
+    figure_usage["data"] = observation_model.get_usage_rate_trace(new_episode)
+    return figure_overflow, figure_usage
 
 @app.callback(
     Output("timeseries_table_micro", "data"),
@@ -10,16 +157,3 @@ from src.grid2viz.utils.graph_utils import relayout_callback
 )
 def sync_timeseries_table(data):
     return data
-
-
-@app.callback(
-    Output("relayoutStoreMicro", "data"),
-    [Input("cum_instant_reward_ts", "relayoutData"),
-     Input("actions_ts", "relayoutData"),
-     Input("interractive_graph", "relayoutData"),
-     Input("temp_id_ts", "relayoutData"),
-     Input("temp_id_heatmap", "relayoutData")],
-    [State("relayoutStoreMicro", "data")]
-)
-def relayout_store_micro(*args):
-    return relayout_callback(*args)
--- src/grid2viz/micro/micro_lyt.py
+++ src/grid2viz/micro/micro_lyt.py
@@ -5,16 +5,18 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import dash_table as dt
 import pandas as pd
+import numpy as np
 
 from src.grid2kpi.episode import observation_model
 from src.grid2kpi.manager import episode, agents, agent_ref
 
 layout_def = {
     'legend': {'orientation': 'h'},
+    "showlegend": True,
     'margin': {'l': 0, 'r': 0, 't': 0, 'b': 0},
 }
 
-indicator_line = html.Div(className="lineBlock card", children=[
+indicator_line = html.Div(id="indicator_line_id", className="lineBlock card", children=[
     html.H4("Indicators"),
     html.Div(className="card-body row", children=[
         html.Div(
@@ -71,79 +73,122 @@ indicator_line = html.Div(className="lineBlock card", children=[
     ])
 ])
 
-flux_inspector_line = html.Div(id="overview_line_id", className="lineBlock card", children=[
-    html.H4("Flux Inspector"),
+flux_inspector_line = html.Div(id="flux_inspector_line_id", className="lineBlock card", children=[
+    html.H4("Flux"),
     html.Div(className="card-body row", children=[
 
         html.Div(className="col-xl-4", children=[
             html.H6(className="text-center",
                     children="Interactive Graph"),
+
             dcc.Loading(
                 dcc.Graph(
-                    id="interractive_graph",
+                    id="temp_id", #TODO don't forget to rename it
                     figure=go.Figure(
                         layout=layout_def,
                     )
                 )
             )
         ]),
-        html.Div(className="col-xl-4", children=[
+        html.Div(className="col-xl-8", children=[
             html.H6(className="text-center",
                     children="Title"),
+            dac.Radio(options=[
+                    {'label': 'Voltage', "value": "voltage"},
+                    {'label': 'Flow', "value": "flow"},
+                ],
+                value="voltage",
+                id="voltage_flow_selector",
+                buttonStyle="solid"
+            ),
+            dac.Select(
+                id="line_side_choices",
+                options=[],
+                value=[],
+                mode='multiple',
+                showArrow=True
+            ),
             dcc.Loading(
                 dcc.Graph(
-                    id="temp_id_ts",
+                    id="voltage_flow_graph",
                     figure=go.Figure(
-                        layout=layout_def
+                        layout=layout_def,
+                        data=[dict(type="scatter")]
                     )
                 ))
         ]),
-        html.Div(className="col-xl-4", children=[
-            html.H6(className="text-center",
-                    children="Title"),
-            dcc.Loading(
-                dcc.Graph(
-                    id="temp_id_heatmap",
-                    figure=go.Figure(
-                        layout=layout_def
-                    )
-                ))
-        ])
     ])
 ])
 
-context_inspector_line = html.Div(className="lineBlock card ", children=[
-    html.H4("Context Inspector"),
+context_inspector_line = html.Div(id="context_inspector_line_id", className="lineBlock card ", children=[
+    html.H4("Context"),
     html.Div(className="card-body col row", children=[
 
-        dac.Radio(options=[
-            {'label': 'Conso', "value": "conso"},
-            {'label': 'Production', "value": "Production"},
-            {'label': 'Hazards', "value": "Hazards"},
-            {'label': 'Maintenances', "value": "Maintenances"},
-        ],
-            value="Load",
-            # name="scen_overview_ts_switch",
-            id="switch_button_context",
-            buttonStyle="solid"
-        ),
-        html.Div(className="col-xl-12", children=[
-            html.H6(className="text-center",
-                    children="Title"),
-            dcc.Loading(
+        html.Div(className="col-xl-5", children=[
+            html.H5("Environments Time Series"),
+            dac.Radio(options=[
+                {'label': 'Load', "value": "Load"},
+                {'label': 'Production', "value": "Production"},
+                {'label': 'Hazards', "value": "Hazards"},
+                {'label': 'Maintenances', "value": "Maintenances"},
+            ],
+                value="Load",
+                id="environment_choices_buttons",
+                buttonStyle="solid"
+            ),
+            dac.Select(
+                id='asset_selector',
+                options=[{'label': load_name,
+                          'value': load_name}
+                         for load_name in episode.load_names],
+                value=episode.load_names[0],
+                mode='multiple',
+                showArrow=True
+            ),
+            dcc.Graph(
+                id='env_charts_ts',
+                style={'margin-top': '1em'},
+                figure=go.Figure(layout=layout_def),
+                config=dict(displayModeBar=False)
+            ),
+        ]),
+
+        html.Div(className="col-xl-7", children=[
+            html.H5("OverFlow and Usage rate"),
+            dcc.Dropdown(
+                id="agent_selector", placeholder="select a ref agent",
+                options=[{'label': agent, 'value': agent} for agent in agents],
+                value=agent_ref
+            ),
+            html.Div(className="row", children=[
                 dcc.Graph(
-                    id="temp_id_heatmap",
+                    id='usage_rate_ts',
+                    className="col-6",
+                    style={'margin-top': '1em'},
                     figure=go.Figure(
-                        layout=layout_def
-                    )
-                ))
-        ])
+                        layout=layout_def,
+                        data=observation_model.get_usage_rate_trace(episode)
+                    ),
+                    config=dict(displayModeBar=False)
+                ),
+                dcc.Graph(
+                    id='overflow_ts',
+                    className="col-6",
+                    style={'margin-top': '1em'},
+                    figure=go.Figure(
+                        layout=layout_def,
+                        data=observation_model.get_total_overflow_trace(episode)
+                    ),
+                    config=dict(displayModeBar=False)
+                ),
+            ], ),
+        ]),
 
     ])
 ])
 
-all_info_line = html.Div(className="lineBlock card ", children=[
-    html.H4("All Informations"),
+all_info_line = html.Div(id="all_info_line_id", className="lineBlock card ", children=[
+    html.H4("All"),
     html.Div(className="card-body col row", children=[
 
         html.Div(className="col-xl-10", children=[
