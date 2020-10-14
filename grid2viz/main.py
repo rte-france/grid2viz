3c6d0ce9bbd12e269ab511d65e72595049220406
marota <antoine-marot@live.fr>
2020-10-14 21:48:32 +0200

    add option to generate cash all at once without launching grid2viz

--- grid2viz/main.py
+++ grid2viz/main.py
@@ -26,6 +26,7 @@ ARG_DEBUG_DESC = 'Enable debug mode for developers.' \
                  ' (default to False)'
 ARG_N_CORES_DESC = 'The number of cores to use for the first loading of the best agents of each scenario'
 
+ARG_CACHE_DESC = 'True if you want to build all the cache data for all agents at once before relaunching grid2viz'
 
 def main():
     parser_main = argparse.ArgumentParser(description='Grid2Viz')
@@ -43,6 +44,8 @@ def main():
 
     parser_main.add_argument('--n_cores', default=2, type=int,
                              help=ARG_N_CORES_DESC)
+    parser_main.add_argument('--cache', default=False, type=bool,
+                             help=ARG_CACHE_DESC)
 
     args = parser_main.parse_args()
 
@@ -69,10 +72,47 @@ def main():
                                            env_dir=env_dir,
                                            n_cores=n_cores))
 
-    # Inline import to load app only now
-    from grid2viz.app import app_run
-    app_run(args.port, args.debug)
+    is_makeCache_only=args.cache
 
+    # Inline import to load app only now
+    if(is_makeCache_only):
+        make_cache()
+    else:
+        from grid2viz.app import app_run
+        app_run(args.port, args.debug)
+
+def make_cache():
+    from grid2viz.src.manager import (scenarios, agents, meta_json,
+                                      make_episode_without_decorate, make_episode,
+                                      n_cores, retrieve_episode_from_disk,
+                                      save_in_ram_cache, cache_dir)
+
+    from pathos.multiprocessing import ProcessPool
+    if not os.path.exists(cache_dir):
+        print('Starting Multiprocessing for reading the best agent of each scenario')
+
+
+    agent_scenario_list=[(agent,scenario) for agent in agents for scenario in scenarios]
+    pool = ProcessPool(n_cores)
+    agents_data = list(pool.imap(make_episode_without_decorate,
+          [agent_scenario[0] for agent_scenario in agent_scenario_list],#agents
+          [agent_scenario[1] for agent_scenario in agent_scenario_list]))#scenarios #we go over all agents and all scenarios for each agent
+    pool.close()
+    print('Multiprocessing done')
+    i=0
+    for agent_scenario in agent_scenario_list:
+        print(i)
+        agent=agent_scenario[0]
+        episode_name=agent_scenario[1]
+        agent_episode = agents_data[i]
+        episode_data = retrieve_episode_from_disk(
+            agent_episode.episode_name, agent_episode.agent)
+
+        agent_episode.decorate(episode_data)
+        save_in_ram_cache(agent_episode.episode_name,
+                              agent_episode.agent,
+                              agent_episode)
+        i+=1
 
 if __name__ == "__main__":
     main()
