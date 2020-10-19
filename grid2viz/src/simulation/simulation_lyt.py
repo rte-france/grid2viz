28fa29f2d40de16b2cba1d3729a2c63600b5e673
Vincent Renault <vincent.renault7@gmail.com>
2020-10-19 16:54:30 +0200

    Improve Choose/Assist layout

--- grid2viz/src/simulation/simulation_lyt.py
+++ grid2viz/src/simulation/simulation_lyt.py
@@ -1,21 +1,88 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_antd_components as dac
+import dash_bootstrap_components as dbc
 
 from grid2viz.src.manager import (agent_scenario, make_network, make_episode)
 
 
-def choose_line(network_graph):
-    return html.Div(id="choose_line", className="lineBlock card", children=[
+def choose_assist_line(episode, network_graph):
+    return html.Div(id="choose_assist_line", className="lineBlock card", children=[
         html.H4("Choose or Assist"),
         html.Div(className="card-body row", children=[
-            html.Div(className="col-9", children=[
+            html.Div(className="col-7", children=[
                 html.H5("Network at time step t"),
-                dcc.Graph(id="network_graph_choose", figure=network_graph),
+                dcc.Graph(id="network_graph_choose", figure=network_graph,),
             ]),
-            html.Div(className="col-3", children=[
-                "content"
-            ])
+            html.Div(className="col-5", children=[
+                dbc.Tabs(children=[
+                    dbc.Tab(label='Choose', labelClassName="fas fa-user", children=[
+                        dbc.Tabs(children=[
+                            dbc.Tab(label="Dropdowns", children=[
+                                dbc.Tabs(children=[
+                                    dbc.Tab(label='Lines', children=[
+                                        html.P("Choose a line to act on:", className="mt-1"),
+                                        dac.Select(
+                                            id='select_lines_simulation',
+                                            options=[{'label': line_name,
+                                                      'value': line_name}
+                                                     for line_name in episode.line_names],
+                                            mode='default',
+                                            value=episode.line_names[0]
+                                        ),
+                                        html.P("Choose an action:", className="mt-1"),
+                                        dac.Radio(options=[
+                                            {"label": "Set", "value": "Set"},
+                                            {"label": "Change", "value": "Change"},
+                                        ], value="Set", id="select_action_lines", buttonStyle="solid"),
+                                    ]),
+                                    dbc.Tab(label='Subs', children=[
+                                        html.P("Choose a substation to act on:"),
+                                        dac.Select(
+                                            id='select_subs_simulation',
+                                            options=[{'label': name,
+                                                      'value': name}
+                                                     for name in episode.name_sub],
+                                            mode='default',
+                                            value=episode.name_sub[0]
+                                        ),
+                                        html.P("Choose an action:"),
+                                        dac.Radio(options=[
+                                            {"label": "Set", "value": "Set"},
+                                            {"label": "Change", "value": "Change"},
+                                        ], value="Set", id="select_action_subs", buttonStyle="solid"),
+                                    ]),
+                                    dbc.Tab(label='Gens', children=[
+                                        html.P("Choose a generator to act on:"),
+                                        dac.Select(
+                                            id='select_gens_simulation',
+                                            options=[{'label': prod_name,
+                                                      'value': prod_name}
+                                                     for prod_name in episode.prod_names],
+                                            mode='default',
+                                            value=episode.prod_names[0]
+                                        ),
+                                        html.P("Choose an action:"),
+                                        dac.Radio(options=[
+                                            {"label": "Redispatch", "value": "Redispatch"},
+                                        ], value="Redispatch", id="select_action_gens", buttonStyle="solid"),
+                                    ])
+                                ])
+                            ]),
+                            dbc.Tab(label="Dict", children=[
+                                html.P("Enter the action dictionary:"),
+                                dbc.Textarea(className="mb-3", placeholder="{set_line_status: []}"),
+                            ])
+                        ]),
+                    ]),
+                    dbc.Tab(label='Assist', labelClassName="fas fa-robot", children=[
+                        "content"
+                    ]),
+                ]),
+                dbc.Button("Simulate", id="simulate_action", color="danger", className="mt-3 mb-3"),
+                html.P("Action dictionary:"),
+                html.Div(id="action_dictionary")
+            ]),
         ]),
     ])
 
@@ -82,13 +149,6 @@ def compare_line(network_graph):
     ])
 
 
-def assist_line():
-    return html.Div(id="assist_line", className="lineBlock card", children=[
-        html.H4("Assist"),
-        html.Div(className="card-body row", children="Content"),
-    ])
-
-
 def layout(scenario, studied_agent):
     episode = make_episode(studied_agent, scenario)
     # TODO : center en correct time step
@@ -96,7 +156,6 @@ def layout(scenario, studied_agent):
         observation=episode.observations[0]
     )
     return html.Div(id="simulation_page", children=[
-        choose_line(network_graph),
+        choose_assist_line(episode, network_graph),
         compare_line(network_graph),
-        assist_line()
     ])
