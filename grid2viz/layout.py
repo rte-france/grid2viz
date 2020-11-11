0b707e326cadf4ab3f1dfc561487f442275db3a8
marota <antoine-marot@live.fr>
2020-11-11 13:29:22 +0100

    added a Help button towards the documentation

--- grid2viz/layout.py
+++ grid2viz/layout.py
@@ -10,7 +10,11 @@ nav_items = [
     dbc.NavItem(dbc.NavLink("Agent Overview",
                             href="/macro", id="nav_agent_over")),
     dbc.NavItem(dbc.NavLink("Agent Study",
-                            href="/micro", id="nav_agent_study"))
+                            href="/micro", id="nav_agent_study")),
+    dbc.NavItem(dbc.NavLink("Help",
+                            href="https://grid2viz.readthedocs.io/en/latest/", id="nav_help", active=True,
+                            target='_blank'))
+                #, style={"background-color": "white"})
 ]
 
 navbar = dbc.Navbar(
@@ -54,7 +58,7 @@ navbar = dbc.Navbar(
             )
         ], id="user_timestamp_div", className="col-xl-1"),
         html.Div(
-            dbc.Nav(nav_items, navbar=True), className="nav_menu"
+            dbc.Nav(nav_items, navbar=True,pills=True), className="nav_menu"
         ),
     ],
     color="#2196F3",
