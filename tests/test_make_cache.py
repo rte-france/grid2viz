6a404fcaa71f8d04aeeb8cb07977678c8e08464d
Vincent Renault <vincent.renault7@gmail.com>
2021-01-18 14:09:15 +0100

    Reformat code of tests and docs conf.py

--- tests/test_make_cache.py
+++ tests/test_make_cache.py
@@ -7,14 +7,10 @@ import unittest
 class TestMakeCache(unittest.TestCase):
     def setUp(self):
         self.agent_path = os.path.join(
-            pathlib.Path(__file__).parent.absolute(),
-            'data', 'agents')
+            pathlib.Path(__file__).parent.absolute(), "data", "agents"
+        )
 
     def test_make_cache(self):
-        cmd = [
-            'grid2viz',
-            '--agents_path', self.agent_path,
-            '--cache', 'True'
-        ]
+        cmd = ["grid2viz", "--agents_path", self.agent_path, "--cache", "True"]
         rv = subprocess.run(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self.assertEqual(rv.returncode, 0)
