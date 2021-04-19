c512d386093093b41226dbe29a8e224e018ca754
Vincent Renault <vincent.renault7@gmail.com>
2021-04-19 15:25:45 +0200

    Update to grid2op@1.5.1
    Fixed a breaking change from Grid2Op (prod_p -> gen_p) and unit tests.
    
    Resolves #90, #69

--- tests/test_make_cache.py
+++ tests/test_make_cache.py
@@ -11,6 +11,9 @@ class TestMakeCache(unittest.TestCase):
         )
 
     def test_make_cache(self):
+        print(self.agent_path)
         cmd = ["grid2viz", "--agents_path", self.agent_path, "--cache", "True"]
-        rv = subprocess.run(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        rv = subprocess.run(
+            cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True
+        )
         self.assertEqual(rv.returncode, 0)
