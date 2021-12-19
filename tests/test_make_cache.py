df0b6d583834818e87b99209587d966c3f68442b
marota <antoine-marot@live.fr>
2021-12-19 14:16:29 +0100

    making test_make_cache work fast

--- tests/test_make_cache.py
+++ tests/test_make_cache.py
@@ -8,7 +8,7 @@ import unittest
 class TestMakeCache(unittest.TestCase):
     def setUp(self):
         self.agent_path = os.path.join(
-            pathlib.Path(__file__).parent.absolute(), "data", "agents"
+            pathlib.Path(__file__).parent.absolute(), "data", "agents_to_cache"
         )
         if os.path.isdir(os.path.join(self.agent_path, "_cache")):
             shutil.rmtree(os.path.join(self.agent_path, "_cache"))
@@ -17,6 +17,6 @@ class TestMakeCache(unittest.TestCase):
         print(self.agent_path)
         cmd = ["grid2viz", "--agents_path", self.agent_path, "--cache"]
         rv = subprocess.run(
-            cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True
+            cmd#, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True
         )
         self.assertEqual(rv.returncode, 0)
