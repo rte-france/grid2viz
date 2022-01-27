f6fa02ca749fb83b01bdeef037bc0de04863a843
marota <antoine-marot@live.fr>
2022-01-27 09:31:52 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,22 +7,22 @@ CHROMEPATH = which("chrome") or which("chromium")
 
 def pytest_setup_options():
 
-    """Simple Function to initialize and configure Webdriver"""
-    if FIREFOXPATH != None:
-        from selenium.webdriver.firefox.options import Options
-        options = Options()
-        print(FIREFOXPATH)#cm
-        options.binary = FIREFOXPATH
-        #options.add_argument("-headless")
-        options.add_argument('--disable-gpu')
-        return options
-
-    elif CHROMEPATH != None:
+    if CHROMEPATH != None:
         from selenium.webdriver.chrome.options import Options
         options = Options()
         print(CHROMEPATH)#cm
 
         options.binary_location = CHROMEPATH
         #options.add_argument("--headless")
+        options.add_argument('--disable-gpu')
+        return options
+    
+        """Simple Function to initialize and configure Webdriver"""
+    elif FIREFOXPATH != None:
+        from selenium.webdriver.firefox.options import Options
+        options = Options()
+        print(FIREFOXPATH)#cm
+        options.binary = FIREFOXPATH
+        #options.add_argument("-headless")
         options.add_argument('--disable-gpu')
         return options
