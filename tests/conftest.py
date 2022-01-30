2ce1eec929a2a0a6e2b8428e79c82b95fc5962ae
marota <antoine-marot@live.fr>
2022-01-30 10:50:06 +0100

    proper conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,7 @@
 # add this in the conftest.py under tests folder
-
-from shutil import which
-FIREFOXPATH = which("firefox")
-CHROMEPATH = which("chrome") or which("chromium")
-
+from selenium.webdriver.chrome.options import Options
 
 def pytest_setup_options():
-
-    if CHROMEPATH != None:
-        from selenium.webdriver.chrome.options import Options
-        options = Options()
-        print(CHROMEPATH)#cm
-
-        options.binary_location = CHROMEPATH
-        #options.add_argument("--headless")
-        options.add_argument('--disable-gpu')
-        return options
-    
-        """Simple Function to initialize and configure Webdriver"""
-    elif FIREFOXPATH != None:
-        from selenium.webdriver.firefox.options import Options
-        options = Options()
-        print(FIREFOXPATH)#cm
-        options.binary = FIREFOXPATH
-        #options.add_argument("-headless")
-        options.add_argument('--disable-gpu')
-        return options
+    options = Options()
+    options.add_argument('--disable-gpu')
+    return options
